fe5f909b4b57dbba95da30d5b9b7b40d7c94e362
Allan Ortíz <7284912+allanortiz@users.noreply.github.com>
2020-05-12 14:01:19 -0500

    Import fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Theme from 'core/theme';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { GlobalStyle } from 'core/coreGlobalStyle';
+import GlobalStyle from 'core/GlobalStyle';
 import Routes from 'core/Routes';
 
 function App() {
