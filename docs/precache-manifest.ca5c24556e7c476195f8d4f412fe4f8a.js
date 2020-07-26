1c2c8571da4a7b01dca046563254d01ef34521ad
Allan Ortíz <7284912+allanortiz@users.noreply.github.com>
2020-07-25 19:18:05 -0500

    Docs update

---
+++ docs/precache-manifest.ca5c24556e7c476195f8d4f412fe4f8a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b45d6a95e1a0080480581b9f5494b67",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fa18bd172fbc830185ac.bundle.js"
+  },
+  {
+    "url": "runtime~main.fa18bd172fbc830185ac.bundle.js"
+  },
+  {
+    "url": "vendors~main.fa18bd172fbc830185ac.bundle.js"
+  },
+  {
+    "url": "vendors~main.fa18bd172fbc830185ac.bundle.js.LICENSE.txt"
+  }
+]);
